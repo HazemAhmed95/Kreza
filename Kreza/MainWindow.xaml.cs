b645cd8d9b19d4800936110f539d57c611aa0307
uonuon <mina.fayek@outlook.com>
2016-05-08 17:20:33 +0200

    Some Changes

--- Kreza/MainWindow.xaml.cs
+++ Kreza/MainWindow.xaml.cs
@@ -24,12 +24,12 @@ namespace Kreza
     /// </summary>
     public partial class MainWindow : Window
     {
-       
+
         public MainWindow()
         {
             InitializeComponent();
             IsPlaying(false);
-          
+
         }
 
         // isPlaying for Play and pause
@@ -49,12 +49,12 @@ namespace Kreza
             IsPlaying(true);
             if (PauseBtn.Content.ToString() == "Pause")
             {
-                ME1.Pause();
+                ME1.LoadedBehavior = MediaState.Pause;
                 PauseBtn.Content = "Play";
             }
             else
             {
-                ME1.Play();
+                ME1.LoadedBehavior = MediaState.Play;
                 PauseBtn.Content = "Pause";
             }
             VolSlider.Value = ME1.Volume;
@@ -62,7 +62,7 @@ namespace Kreza
         // stop btn 
         private void StopBtn_Click(object sender, RoutedEventArgs e)
         {
-            ME1.Stop();
+            ME1.LoadedBehavior = MediaState.Stop;
             PauseBtn.Content = "Play";
         }
 
@@ -98,54 +98,48 @@ namespace Kreza
             }
         }
 
-        StreamReader FileReader; 
+        StreamReader FileReader;
         string line;
-        int padding;
-        
+
+
         SearchSongsAndDuplication sr = new SearchSongsAndDuplication();
-        
+
         private void ViewAllSongs(object sender, RoutedEventArgs e)//Viewing all songs.
         {
             FileReader = new StreamReader("All Songs.txt");//Accessing AllSongs file.
 
             while ((line = FileReader.ReadLine()) != null) //Looking for the songs on the file.
             {
-                //string Spaces ="";
                 string[] splitter = line.Split('|');
-                //padding = 80 - splitter[0].Length;
-               
-                //for (int i = 0; i < padding; i++)//Making spaces for the margins.
-                //{
-                //    Spaces += " ";
-                //}
-                int cutter=0;
-              for(int i = 0; i < splitter[0].Length; i++)
-                  {
-               if(splitter[0][i] == '.'){
-                  cutter = i + 1;
-                  break;
-                    }	
-                   }
 
-            splitter[0] = splitter[0].Substring(0,cutter);
+                int cutter = 0;
+                for (int i = 0; i < splitter[0].Length; i++)
+                {
+                    if (splitter[0][i] == '.' && splitter[0][i + 1] == 'm')
+                    {
+                        cutter = i;
+                        break;
+                    }
+                }
+                splitter[0] = splitter[0].Substring(0, cutter);
                 SongsList.Items.Add(splitter[0]);//Adding the songs to the list.
             }
-           
+
             FileReader.Close();//Closing the file.
         }
-        
+
         private void OpeningFileDialog(object sender, RoutedEventArgs e)//Opening file dialog.
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Multiselect = true;
             openFileDialog.Filter = "All Files|*.*";
-            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);                       
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             if (openFileDialog.ShowDialog() == true)
             {
                 string filename = openFileDialog.FileName;
                 string path = System.IO.Path.GetDirectoryName(filename);
                 sr.SearchDirectory(path);      //Searching in directory and subdirectories.
-                
+
             }
         }
 
@@ -154,7 +148,7 @@ namespace Kreza
         private void SongsList_MouseDoubleClick(object sender, MouseButtonEventArgs e)//Double clicked items.
         {
             string Selected = SongsList.SelectedItem.ToString();//getting the item and change it to string.
-            Selected = Selected + "mp3";
+            Selected = Selected + ".mp3";
             FileReader = new StreamReader("All Songs.txt");//Accessing the file
 
             while ((line = FileReader.ReadLine()) != null)
@@ -166,25 +160,25 @@ namespace Kreza
                     Uri path = new Uri(splitter[3]);//Giving it it's path
                     ME1.Source = path;
                     ME1.LoadedBehavior = MediaState.Play; //play the song.
-                    
+
                     try
                     { //Getting the Album art source
-                    TagLib.File tagFile = TagLib.File.Create(splitter[3]);
-                    TagLib.IPicture pic = tagFile.Tag.Pictures[0];
-                    MemoryStream ms = new MemoryStream(pic.Data.Data);
-                    ms.Seek(0, SeekOrigin.Begin);
+                        TagLib.File tagFile = TagLib.File.Create(splitter[3]);
+                        TagLib.IPicture pic = tagFile.Tag.Pictures[0];
+                        MemoryStream ms = new MemoryStream(pic.Data.Data);
+                        ms.Seek(0, SeekOrigin.Begin);
 
-                    // ImageSource 
-                    BitmapImage bitmap = new BitmapImage();
-                    bitmap.BeginInit();
-                    bitmap.StreamSource = ms;
-                    bitmap.EndInit();
+                        // ImageSource 
+                        BitmapImage bitmap = new BitmapImage();
+                        bitmap.BeginInit();
+                        bitmap.StreamSource = ms;
+                        bitmap.EndInit();
 
-                    
-                    AlbumArt.Source = bitmap; 
+
+                        AlbumArt.Source = bitmap;
                     }
                     catch (Exception)// exception if there is no Album art.
-                    { 
+                    {
                         BitmapImage image = new BitmapImage(new Uri(@"\Assets\no_album_art_by_gouki113.png", UriKind.Relative));
                         AlbumArt.Source = image;
                     }
@@ -199,9 +193,9 @@ namespace Kreza
 
         }
 
-       
-              
-        
-        
+
+
+
+
     }
 }
